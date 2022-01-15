7d274530039137e843caef809bc035773d4d9d82
Sabrina Isomoto <sabrina.isomoto@gmail.com>
2022-01-15 15:04:34 -0300

    Props

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,5 +1,5 @@
 import axios, { Axios } from "axios";
-import { useState , useEffect } from "react";
+import { useState, useEffect } from "react";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/pagination";
 import { BASE_URL } from "utils/requests";
@@ -9,14 +9,25 @@ function Listing() {
 
     const [pageNumber, setPageNumber] = useState(0);
 
-    useEffect(() => {
-    axios.get(`${BASE_URL}/movies?size=12&page=3`)
-    .then(response => {
-    const data = response.data as MoviePage;
-    console.log(data);
-    setPageNumber(data.number);
+    const [page, setPage] = useState<MoviePage>({
+        content: [],
+        last: true,
+        totalPages: 0,
+        totalElements: 0,
+        size: 12,
+        number: 0,
+        first: true,
+        numberOfElements: 0,
+        empty: true
     });
-}, []);
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber} &sort=id`)
+            .then(response => {
+                const data = response.data as MoviePage;
+                setPage(data);
+            });
+    }, [pageNumber]);
 
     // forma entrada, lógica solta e fora do ciclo do react, faz com que ele roda duas vezes
     //axios.get(`${BASE_URL}/movies?size=12&page=3`)
@@ -27,28 +38,18 @@ function Listing() {
 
     return (
         <>
-        <p>{pageNumber}</p>
             <Pagination />
 
             <div className="container">
                 <div className="row">
 
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb=3">
-                        <MovieCard />
-                    </div>
+                    {page.content.map(movie => (
+                        <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb=3">
+                            <MovieCard movie={movie} />
+                        </div>
+                    )
+                    )}
 
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb=3">
-                        <MovieCard />
-                    </div>
-
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb=3">
-                        <MovieCard />
-                    </div>
-
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb=3">
-                        <MovieCard />
-                    </div>
-                    
                 </div>
             </div>
         </>
